6c13a30d926e13d564ce366b83dd6bf355f98375
ainan-ahmed <a.ahmed.chy@gmail.com>
2020-03-25 00:10:44 +0600

    updated 25.2.2020:12.10

--- products/views.py
+++ products/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from .models import Category
 # Create your views here.
 def home (request):
     return render(request,'products/home.html')
+
+@login_required
+def create(request):
+    # add url in the product model
+    categories = Category.objects.all()
+    return render(request,'products/create.html',{'categories':categories})
