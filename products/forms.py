292d3d2fefb8b216d3e4d2b673da1c98fa763a1b
ainan-ahmed <a.ahmed.chy@gmail.com>
2020-03-29 23:48:56 +0600

    added django.forms instead of validating manually

---
+++ products/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Product,Category
+
+class ProductCreateForm(forms.ModelForm):
+    category  = forms.ModelChoiceField(queryset=Category.objects.all())
+    
+    class Meta:
+        model = Product
+        fields = {'title','image','icon','body','category'}
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'input--style-5'}),
+            'body':forms.Textarea(attrs={'class':'input--style-5','col':40,'row':20})
+        }
+        
+
--- products/views.py
+++ products/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Category,Product
 from django.utils import timezone
+from .forms import ProductCreateForm
+from django.contrib import messages
 # Create your views here.
 def home (request):
     products = Product.objects.all()
@@ -10,36 +12,44 @@ def home (request):
 @login_required
 def create(request):
     if request.method=='POST':
-        category  = Category.objects.get(name = request.POST['category'])
-        product = Product()
-        product.title = request.POST['title']
-        product.body = request.POST['body']
-        product.category = category
-        product.icon = request.FILES['icon']
-        product.image = request.FILES['image']
-        product.publication_date = timezone.datetime.now()
-        product.hunter = request.user 
-        product.save()
-        return redirect('home')
+        form = ProductCreateForm(request.POST,request.FILES)
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.publication_date = timezone.datetime.now() 
+            product.hunter = request.user
+            product.save()
+            return redirect('/products/'+ str(product.id))
     else:
-        categories = Category.objects.all()
-        return render(request,'products/create.html',{'categories':categories})
-    
+        form = ProductCreateForm()
+        return render(request,'products/create.html',{'form':form})
+
 def show(request,product_id):
     product = get_object_or_404(Product,pk = product_id)
     return render(request,'products/show.html',{'product':product})
+
 @login_required
 def upvote(request,product_id):
     product = Product.objects.get(id = product_id)
     if product.hunter == request.user:
-        upvote_error = "You can't upvote your own product"
+        messages.error(request,"You can't upvote your own product")
         return redirect('/products/'+ str(product_id))
     product.total_votes +=1
     product.save()
     return redirect('/products/'+ str(product_id))
+
 @login_required
 def edit(request,product_id):
-    print('adsf')
+    if request.method == 'GET':
+        product = get_object_or_404(Product,pk = product_id)
+        form = ProductCreateForm(instance=product)
+        return render(request,'products/edit.html',{'form':form,'product':product})
+    else:
+        form = ProductCreateForm(request.POST,request.FILES,instance=product)
+        if form.is_valid():
+            product = Product.objects.get(id = product_id)
+            form.save()
+            return redirect('/products/'+ str(product_id))
+
 @login_required
 def delete(request,product_id):
     print('adsf')
